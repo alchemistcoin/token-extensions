8c0bddc0e8d072dbb4b10b758aaed98f082ae523
kryptachio <97916577+kryptachio@users.noreply.github.com>
2022-06-15 11:55:41 +0100

    COP-1159: create Erc721BurningErc20OnMint contract (#2)

---
+++ contracts/IErc721BurningErc20OnMint.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+interface IErc721BurningErc20OnMint {
+    function setErc20TokenAddress(address erc20TokenAddress_) external;
+
+    function mint(address to) external;
+}
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const TestERC721Factory = await ethers.getContractFactory("TestERC721");
+  const testERC721 = await TestERC721Factory.deploy();
 
-  await greeter.deployed();
+  await testERC721.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("TestERC721 deployed to:", testERC721.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
